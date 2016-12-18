abf8f0d81b934a299eae1a9059a37b289ab9ae6b
Shadowfacts <me@shadowfacts.net>
2016-12-18 09:46:31 -0500

    Fix NBT tag sometimes being invalid

--- src/main/kotlin/net/shadowfacts/redstoneremote/util/Extensions.kt
+++ src/main/kotlin/net/shadowfacts/redstoneremote/util/Extensions.kt
@@ -7,21 +7,37 @@ import net.minecraft.nbt.NBTTagCompound
  * @author shadowfacts
  */
 fun ItemStack.getStrength(): Int {
-	if (!hasTagCompound()) return 15
+	if (!hasTagCompound()) {
+		tagCompound = NBTTagCompound()
+		tagCompound!!.setInteger("strength", 15)
+		tagCompound!!.setInteger("duration", 3)
+		return 15
+	}
 	return tagCompound!!.getInteger("strength")
 }
 
 fun ItemStack.setStrength(strength: Int) {
-	if (!hasTagCompound()) tagCompound = NBTTagCompound()
+	if (!hasTagCompound()) {
+		tagCompound = NBTTagCompound()
+		tagCompound!!.setInteger("duration", 3)
+	}
 	tagCompound!!.setInteger("strength", strength)
 }
 
 fun ItemStack.getDuration(): Int {
-	if (!hasTagCompound()) return 3
+	if (!hasTagCompound()) {
+		tagCompound = NBTTagCompound()
+		tagCompound!!.setInteger("strength", 15)
+		tagCompound!!.setInteger("duration", 3)
+		return 3
+	}
 	return tagCompound!!.getInteger("duration")
 }
 
 fun ItemStack.setDuration(duration: Int) {
-	if (!hasTagCompound()) tagCompound = NBTTagCompound()
+	if (!hasTagCompound()) {
+		tagCompound = NBTTagCompound()
+		tagCompound!!.setInteger("strength", 15)
+	}
 	tagCompound!!.setInteger("duration", duration)
 }
